1546611715d53b04fa3967591ec73c5e8bee556c
gduminy <guillaume.duminy@etu.unilim.fr>
2018-03-25 16:25:32 -0400

    creation de du premier ecran et changement d'activité quand on appuie sur le bouton créer

---
+++ app/src/main/java/ca/uqac/projetjdr/CreationActivite.java
@@ -0,0 +1,20 @@
+package ca.uqac.projetjdr;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+/**
+ * Created by guillaume on 25/03/2018.
+ */
+
+public class CreationActivite extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.creation_activity);
+    }
+
+
+}
