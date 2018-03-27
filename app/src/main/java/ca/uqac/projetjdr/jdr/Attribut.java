c8b1499d163c227b4ceeadee91d7c5f84a79fcc1
Fiesta87 <jb@pommeret.fr>
2018-03-27 00:52:46 -0400

    variable privée avec getter et setter et levé d'exception si non-conforme

--- app/src/main/java/ca/uqac/projetjdr/jdr/Attribut.java
+++ app/src/main/java/ca/uqac/projetjdr/jdr/Attribut.java
@@ -3,13 +3,40 @@ package ca.uqac.projetjdr.jdr;
 import java.util.ArrayList;
 import java.util.List;
 
+import ca.uqac.projetjdr.jdr.exception.ValeurImpossibleException;
+
 public class Attribut<T> {
 
-    public String nom;
-    public T valeur;
-    public List<Attribut> listeSousAttributs;
+    private String nom;
+    private T valeur;
+    private List<Attribut> listeSousAttributs;
 
-    public Attribut(){
+    public Attribut(String nom, T valeur) throws ValeurImpossibleException {
+        this.setNom(nom);
+        this.valeur = valeur;
         this.listeSousAttributs = new ArrayList<>();
+    }
+
+    public String getNom() {
+        return this.nom;
+    }
+
+    public void setNom(String nom) throws ValeurImpossibleException {
+        if(nom == null || nom.equals("")){
+            throw new ValeurImpossibleException("Le nom de l'attribut doit être renseigné.");
+        }
+        this.nom = nom;
+    }
+
+    public T getValeur() {
+        return valeur;
+    }
+
+    public void setValeur(T valeur) {
+        this.valeur = valeur;
+    }
+
+    public List<Attribut> getListeSousAttributs() {
+        return listeSousAttributs;
     }
 }
--- app/src/main/java/ca/uqac/projetjdr/jdr/FichePersonnage.java
+++ app/src/main/java/ca/uqac/projetjdr/jdr/FichePersonnage.java
@@ -1,8 +1,32 @@
 package ca.uqac.projetjdr.jdr;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import ca.uqac.projetjdr.jdr.exception.ValeurImpossibleException;
 
 public class FichePersonnage {
 
-    public List<Attribut> listeAttributs;
+    private String nomPersonnage;
+    private List<Attribut> listeAttributs;
+
+    public FichePersonnage(String nomPersonnage) throws ValeurImpossibleException {
+        this.setNomPersonnage(nomPersonnage);
+        this.listeAttributs = new ArrayList<>();
+    }
+
+    public String getNomPersonnage() {
+        return this.nomPersonnage;
+    }
+
+    public void setNomPersonnage(String nomPersonnage) throws ValeurImpossibleException {
+        if(nomPersonnage == null || nomPersonnage.equals("")){
+            throw new ValeurImpossibleException("Le nom du personnage doit être renseigné.");
+        }
+        this.nomPersonnage = nomPersonnage;
+    }
+
+    public List<Attribut> getListeAttributs() {
+        return this.listeAttributs;
+    }
 }
