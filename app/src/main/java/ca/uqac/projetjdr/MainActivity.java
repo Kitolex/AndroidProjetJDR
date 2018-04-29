c8ae37cec3e69a28d19d5510dddf87962a15fe03
Fiesta87 <jb@pommeret.fr>
2018-04-29 16:52:47 -0400

    début modifiacation du design

--- app/src/main/java/ca/uqac/projetjdr/MainActivity.java
+++ app/src/main/java/ca/uqac/projetjdr/MainActivity.java
@@ -6,21 +6,9 @@ import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View;
-
-import org.xml.sax.SAXException;
-
-import java.io.IOException;
-
-import javax.xml.parsers.ParserConfigurationException;
-
-import ca.uqac.projetjdr.util.NoeudXML;
-import ca.uqac.projetjdr.util.XMLUtil;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final int READ_REQUEST_CODE = 42;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,13 +23,8 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void BoutonModifier(View view) {
+    public void BoutonMesFiches(View view) {
         Intent intent = new Intent(MainActivity.this, ListeFichesActivity.class);
-        startActivity(intent);
-    }
-
-    public void BoutonSupprimer(View view) {
-        Intent intent = new Intent(MainActivity.this, SupprimerActivite.class);
         startActivity(intent);
     }
 
@@ -54,53 +37,4 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this, LancerDesActivity.class);
         startActivity(intent);
     }
-
-    public void readXML(View v){
-        try {
-            XMLUtil xml = new XMLUtil((getAssets().open("Test.xml")));
-            NoeudXML res = xml.lireXML();
-            System.out.println(res.toString());
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (SAXException e) {
-            e.printStackTrace();
-        } catch (ParserConfigurationException e) {
-            e.printStackTrace();
-        }
-    }
-
-      /*  public void readXML(View v){
-        try {
-            XMLUtil xml = new XMLUtil((getAssets().open("Test.xml")));
-            NoeudXML res = xml.lireXML();
-            System.out.println(res.getNom());
-            if(res.haveSousElement()){
-                ArrayList<NoeudXML> temp = res.getListNoeud();
-                for (NoeudXML test: temp) {
-                    System.out.println(test.getNom());
-                    if(res.haveSousElement()){
-                        ArrayList<NoeudXML> temp2 = test.getListNoeud();
-                        for (NoeudXML test2: temp2) {
-                            System.out.println(test2.getNom());
-                        }
-                    }
-                }
-
-
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (SAXException e) {
-            e.printStackTrace();
-        } catch (ParserConfigurationException e) {
-            e.printStackTrace();
-        }
-    }
-*/
-
-
-
-
-
 }
