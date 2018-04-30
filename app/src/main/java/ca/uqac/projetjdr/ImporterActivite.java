5e89950e3cb339a9c7bed88a0375ea4c720ad29b
Lucas <kitolex.soluce@gmail.com>
2018-04-30 15:17:57 -0400

    importation xml

--- app/src/main/java/ca/uqac/projetjdr/ImporterActivite.java
+++ app/src/main/java/ca/uqac/projetjdr/ImporterActivite.java
@@ -2,10 +2,18 @@ package ca.uqac.projetjdr;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.OpenableColumns;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 /**
  * Created by guillaume on 25/03/2018.
@@ -18,10 +26,11 @@ public class ImporterActivite extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent getContentIntent = FileUtils.createGetContentIntent();
 
         Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
         intent.addCategory(Intent.CATEGORY_OPENABLE);
-        intent.setType("text/xml");
+        intent.setType("*/*");
         startActivityForResult(intent, READ_REQUEST_CODE);
     }
 
@@ -34,13 +43,65 @@ public class ImporterActivite extends AppCompatActivity {
             if (resultData != null) {
                 uri = resultData.getData();
                 Log.i("TAG", "Uri: " + uri.toString());
+                try {
+                    copyFile(uri);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
             }
         }
 
     }
 
+    void copyFile(Uri uri) throws IOException {
+        File f = getFilesDir();
+        System.out.println(f.listFiles().length);
+        String sourceFilename = uri.getPath();
+        String destinationFilename = getFilesDir().getAbsolutePath()+"/"+getFileName(uri);
 
 
+
+        InputStream in = getContentResolver().openInputStream(uri);
+        OutputStream out = new FileOutputStream(new File(destinationFilename));
+
+        // Copy the bits from instream to outstream
+        byte[] buf = new byte[1024];
+        int len;
+        while ((len = in.read(buf)) > 0) {
+            out.write(buf, 0, len);
+        }
+        in.close();
+        out.close();
+
+
+
+        System.out.println(f.listFiles().length);
+    }
+
+    public String getFileName(Uri uri) {
+        String result = null;
+        if (uri.getScheme().equals("content")) {
+            Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                }
+            } finally {
+                cursor.close();
+            }
+        }
+        if (result == null) {
+            result = uri.getPath();
+            int cut = result.lastIndexOf('/');
+            if (cut != -1) {
+                result = result.substring(cut + 1);
+            }
+        }
+        System.out.println(result);
+        return result;
+
+    }
 
 
 
