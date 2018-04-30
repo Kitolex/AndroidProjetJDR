feb4a0f1c5983b8b343fd47549ec7c4ecb47c794
Lucas <kitolex.soluce@gmail.com>
2018-04-30 14:32:55 -0400

    debut importation xml

--- app/src/main/java/ca/uqac/projetjdr/ImporterActivite.java
+++ app/src/main/java/ca/uqac/projetjdr/ImporterActivite.java
@@ -1,8 +1,11 @@
 package ca.uqac.projetjdr;
 
-import android.content.pm.ActivityInfo;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 /**
  * Created by guillaume on 25/03/2018.
@@ -10,11 +13,36 @@ import android.support.v7.app.AppCompatActivity;
 
 public class ImporterActivite extends AppCompatActivity {
 
+    private static final int READ_REQUEST_CODE = 1234;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.importer_activity);
 
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("text/xml");
+        startActivityForResult(intent, READ_REQUEST_CODE);
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode,
+                                 Intent resultData) {
+        if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+
+            Uri uri = null;
+            if (resultData != null) {
+                uri = resultData.getData();
+                Log.i("TAG", "Uri: " + uri.toString());
+            }
+        }
+
+    }
+
+
+
+
+
+
+
 }
